33e5a449bdbbe7ae63ccca6cd3afd97c4143ab48
AlexeyPuchko <alexey.puchko@itechart-group.com>
2015-03-14 19:29:56 +0300

    Added AppConfig class

---
+++ Project/app/src/main/java/com/alexeypuchko/project/AppConfig.java
@@ -0,0 +1,13 @@
+package com.alexeypuchko.project;
+
+/**
+ * Created by Alexey on 14.03.2015.
+ */
+public class AppConfig {
+
+    // IS DEBUG?
+    public static final boolean DEBUG_BUILD = true;
+    // Network connection timeouts
+    public static final int CONNECT_TIMEOUT_SECONDS = 30;
+    public static final int READ_TIMEOUT_SECONDS = 30;
+}
